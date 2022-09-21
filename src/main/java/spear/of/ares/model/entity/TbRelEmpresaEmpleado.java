be88e63e360fe266068966e893f7b4d8b53f4192
Miguel Á. Sastre <sastre113@gmail.com>
2022-09-21 23:56:16 +0200

    Actualizado entidad que relaciona Empleado y Empresa

--- src/main/java/spear/of/ares/model/entity/TbRelEmpresaEmpleado.java
+++ src/main/java/spear/of/ares/model/entity/TbRelEmpresaEmpleado.java
@@ -6,7 +6,6 @@ package spear.of.ares.model.entity;
 import java.util.Date;
 
 import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -19,16 +18,68 @@ import lombok.Data;
  *
  */
 @Entity
-@Table(name="TB_REL_EMPRESA_EMPLEADO")
+@Table(name = "TB_REL_EMPRESA_EMPLEADO")
 @Data
 public class TbRelEmpresaEmpleado {
 
-	@EmbeddedId
-	private TbRelEmpresaEmpleadoPK id;
-	@Column(name="tipo_puesto", updatable=false, nullable = false, length=50)
+	@Column(name = "id_relacion", updatable = false, nullable = false, length = 36)
+	private String idRelacion;
+	@Column(name = "id_empresa", updatable = false, nullable = false, length = 36)
+	private String idEmpresa;
+	@Column(name = "id_empleado", updatable = false, nullable = false, length = 36)
+	private String idEmpleado;
+	@Column(name = "tipo_puesto", updatable = false, nullable = false, length = 50)
 	private String tipoPuesto;
-	@Column(name="fecha_alta", updatable=false, nullable = false)
+	@Column(name = "fecha_alta", updatable = false, nullable = false)
 	private Date fechaAlta;
-	@Column(name="fecha_baja", updatable=false, nullable = false)
+	@Column(name = "fecha_baja", updatable = false, nullable = false)
 	private Date fechaBaja;
+
+	public String getIdRelacion() {
+		return idRelacion;
+	}
+
+	public void setIdRelacion(String idRelacion) {
+		this.idRelacion = idRelacion;
+	}
+
+	public String getIdEmpresa() {
+		return idEmpresa;
+	}
+
+	public void setIdEmpresa(String idEmpresa) {
+		this.idEmpresa = idEmpresa;
+	}
+
+	public String getIdEmpleado() {
+		return idEmpleado;
+	}
+
+	public void setIdEmpleado(String idEmpleado) {
+		this.idEmpleado = idEmpleado;
+	}
+
+	public String getTipoPuesto() {
+		return tipoPuesto;
+	}
+
+	public void setTipoPuesto(String tipoPuesto) {
+		this.tipoPuesto = tipoPuesto;
+	}
+
+	public Date getFechaAlta() {
+		return fechaAlta;
+	}
+
+	public void setFechaAlta(Date fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
+
+	public Date getFechaBaja() {
+		return fechaBaja;
+	}
+
+	public void setFechaBaja(Date fechaBaja) {
+		this.fechaBaja = fechaBaja;
+	}
 }
