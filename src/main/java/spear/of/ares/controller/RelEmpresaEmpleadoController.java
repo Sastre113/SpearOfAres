f0f4109778e7bdc2654ffe5af7a08f96b79644cd
Miguel Á. Sastre <sastre113@gmail.com>
2022-09-28 20:39:19 +0200

    Eliminada redundancia en DTOs.

--- src/main/java/spear/of/ares/controller/RelEmpresaEmpleadoController.java
+++ src/main/java/spear/of/ares/controller/RelEmpresaEmpleadoController.java
@@ -3,19 +3,8 @@
  */
 package spear.of.ares.controller;
 
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import spear.of.ares.excepcion.AresException;
-import spear.of.ares.model.dto.peticion.Peticion;
-import spear.of.ares.model.dto.peticion.Respuesta;
-import spear.of.ares.model.dto.relEmpresaEmpleado.RelEmpresaEmpleadoDTO;
-import spear.of.ares.model.dto.relEmpresaEmpleado.RespuestaRelEmpresaEmpleadoDTO;
-import spear.of.ares.service.IRelEmpresaEmpleado;
 
 /**
  * @author Miguel Á. Sastre <sastre113@gmail.com>
@@ -26,20 +15,4 @@ import spear.of.ares.service.IRelEmpresaEmpleado;
 @RequestMapping("/gestionRelacionEmpresaEmpleado")
 public class RelEmpresaEmpleadoController {
 
-	private IRelEmpresaEmpleado relEmpresaEmpleadoService;
-
-	public RelEmpresaEmpleadoController(IRelEmpresaEmpleado relEmpresaEmpleadoService) {
-		super();
-		this.relEmpresaEmpleadoService = relEmpresaEmpleadoService;
-	}
-	
-	
-	@PostMapping(path = "/insertarEmpresa", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Respuesta<String, RespuestaRelEmpresaEmpleadoDTO>> insertarEmpresa(@RequestBody Peticion<String, RelEmpresaEmpleadoDTO> peticionDTO) {	
-		try {
-			return ResponseEntity.ok(this.relEmpresaEmpleadoService.insertar(peticionDTO));
-		} catch (AresException e) {
-			return ResponseEntity.status(e.getHttpStatus()).body(e.mapError(Respuesta.class));
-		}	 
-	}
 }
--- src/main/java/spear/of/ares/dao/IEmpleadoDAO.java
+++ src/main/java/spear/of/ares/dao/IEmpleadoDAO.java
@@ -4,6 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 
 import spear.of.ares.model.entity.TbEmpleado;
 
+/** 
+ * @author Miguel Á. Sastre <sastre113@gmail.com>
+ * @version 20:22:32 - 28/09/2022
+ */
 public interface IEmpleadoDAO extends CrudRepository<TbEmpleado, String> {
 
 }
